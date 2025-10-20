71c4ee175e636456d214b8f421927034284041ad
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2025-10-20 22:15:23 +0300

    project init

---
+++ api/commitlint.config.js
@@ -0,0 +1,18 @@
+export default {
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "add", "docs", "style", "refactor", "test", "chore"],
+    ],
+    "subject-empty": [2, "never"],
+    "type-case": [2, "always", "lower-case"],
+  },
+  parserPreset: {
+    parserOpts: {
+      // Match either [type]: subject OR type: subject
+      headerPattern: /^\[?(\w+)\]?:\s(.+)$/,
+      headerCorrespondence: ["type", "subject"],
+    },
+  },
+};
